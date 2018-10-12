e9464dc9c5bab37a2ae529ed5b31473f2d4822eb
Pawel Wegrecki <p.wegrecki1@gmail.com>
2018-10-12 13:36:52 +0200

    Add Book model. Add jackson-databind dependency

--- src/main/java/pl/coderslab/controller/BookController.java
+++ src/main/java/pl/coderslab/controller/BookController.java
@@ -3,13 +3,24 @@ package pl.coderslab.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.coderslab.model.Book;
 
 @RestController
 @RequestMapping("/books")
 public class BookController {
 
+
+
     @GetMapping("/hello")
     public String hello() {
         return "{msg: hello}";
     }
+
+    @GetMapping("/helloBook")
+    public Book helloBook(){
+        return new Book(1L,"9788324631766","Thinking in Java",
+                "Bruce Eckel","Helion","programming");
+    }
+
+
 }
